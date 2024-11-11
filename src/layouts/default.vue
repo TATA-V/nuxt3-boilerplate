ce8cce37c52461a9c10bf13447455784c246cc26
tatahyeonv <xhrl523@naver.com>
2024-11-11 21:42:21 +0900

    chore

---
+++ src/layouts/default.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div className="flex justify-center w-full">
+    <div className="w-[1240px] px-5">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
